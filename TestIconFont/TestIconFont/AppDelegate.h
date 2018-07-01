4ad195a85578476554b49c97705bc557dc94f8be
sjz <2528458740@qq.com>
2018-07-01 16:11:54 +0800

    By 2018.7.1

---
+++ TestIconFont/TestIconFont/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestIconFont
+//
+//  Created by os on 2018/7/1.
+//  Copyright © 2018年 os. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
